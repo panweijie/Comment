0f3bf06f7279f29928c1aa566fca7270b430b0d1
terryHwang <543695151@qq.com>
2018-08-23 09:41:24 +0800

    second commit

---
+++ src/main/java/com/oceanworld/common/config/datasource/DruidDBConfig.java
@@ -0,0 +1,39 @@
+package com.oceanworld.common.config.datasource;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+@Configurable
+public class DruidDBConfig {
+    private Logger logger = LoggerFactory.getLogger(DruidDBConfig.class);
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClass;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    @Primary
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+}
